adb0477eb8e11e319d798d2be07f6fd7dc204704
AuYang <459461160@qq.com>
2023-09-26 12:55:06 +0800

    更新命名和目录结构；添加快速运行脚本run.bat

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # BIT_yanhe_download
 
  ## 依赖
- 
+
  * python第三方库： 
  ```
  requests、execjs
@@ -24,14 +24,20 @@ git clone https://github.com/AuYang261/BIT_yanghe_download.git
 python main.py 39134
 ```
 
+或双击运行`run.bat`文件，并输入课程编号
+
 输出课程视频列表：
 
-![image-20230331172059079](md/README/image-20230331172059079.png)
+![image-20230926124749421](md/README/image-20230926124749421.png)
 
-输入想下载的视频编号，用英文逗号(,)分隔，回车。接着选择下载video视频录像（即教室后的摄像头录像）还是下载vga信号（即教室电脑的屏幕），默认为视频录像。回车即开始下载：
+输入想下载的视频编号，用英文逗号(,)分隔，回车。接着选择下载video视频录像（即教室后的摄像头录像）还是下载screen信号（即教室电脑的屏幕），默认为视频录像。回车即开始下载：
 
-![image-20230331173242553](md/README/image-20230331173242553.png)
+![image-20230926124841432](md/README/image-20230926124841432.png)
 
-下载完成的文件在同级目录下以课程名-video/vga格式命名的文件夹中。
+下载完成的文件在`output/`目录下以课程名-video/screen格式命名的文件夹中。
 
-![image-20230331172530624](md/README/image-20230331172530624.png)
+![image-20230926124922726](md/README/image-20230926124922726.png)
+
+## 注意
+
+* 需要关闭本机上的代理，否则会提示类似`ValueError: check_hostname requires server_hostname`的报错信息。
--- main.py
+++ main.py
@@ -2,34 +2,42 @@ import requests
 import m3u8dl
 import sys
 import json
+import os
 headers={
-'Origin': 'https://www.yanhekt.cn',
-"xdomain-client": "web_user",
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'
+    'Origin': 'https://www.yanhekt.cn',
+    "xdomain-client": "web_user",
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26'
 }
 
 
 # courseID = 31425
 if __name__ == '__main__':
-	courseID = sys.argv[1]
+    if len(sys.argv) == 1:
+        courseID = eval(input('Please input course id: '))
+    else:
+        courseID = sys.argv[1]
 
-	course = requests.get(f'https://cbiz.yanhekt.cn/v1/course?id={courseID}&with_professor_badges=true', headers=headers)
-	req = requests.get(f'https://cbiz.yanhekt.cn/v2/course/session/list?course_id={courseID}', headers=headers)
-	print(course.json()['data']['name_zh'])
-	videoList = req.json()['data']
-	# print(json.dumps(videoList, indent=2))
-	for i, c in enumerate(videoList):
-		print(i, c['title'])
+    course = requests.get(f'https://cbiz.yanhekt.cn/v1/course?id={courseID}&with_professor_badges=true', headers=headers)
+    req = requests.get(f'https://cbiz.yanhekt.cn/v2/course/session/list?course_id={courseID}', headers=headers)
+    print(course.json()['data']['name_zh'])
+    videoList = req.json()['data']
+    # print(json.dumps(videoList, indent=2))
+    for i, c in enumerate(videoList):
+        print(i, c['title'])
 
-	index = eval('[' + input('select(split by \',\'):') + ']')
-	vga = input('video(1) or vga(2)?(input 1 or 2, default 1):')
-	for i in index:
-		c = videoList[i]
-		name = course.json()['data']['name_zh'] + '-' + course.json()['data']['professors'][0]['name'] + '-' + c['title']
-		print(name)
-		if vga == "2":
-			m3u8dl.M3u8Download(c['videos'][0]['vga'], course.json()['data']['name_zh'] + ('-vga' if vga == '2' else '-video'), name)
-		else:
-			m3u8dl.M3u8Download(c['videos'][0]['main'], course.json()['data']['name_zh'] + ('-vga' if vga == '2' else '-video'), name)
+    index = eval('[' + input('select(split by \',\'):') + ']')
+    vga = input('video(1) or screen(2)?(input 1 or 2, default 1):')
+    if not os.path.exists('output/'):
+        os.mkdir('output/')
+    for i in index:
+        c = videoList[i]
+        name = course.json()['data']['name_zh'] + '-' + course.json()['data']['professors'][0]['name'] + '-' + c['title']
+        print(name)
+        if vga == "2":
+            print("Downloading screen...")
+            m3u8dl.M3u8Download(c['videos'][0]['vga'], 'output/' + course.json()['data']['name_zh'] + ('-screen' if vga == '2' else '-video'), name)
+        else:
+            print("Downloading video...")
+            m3u8dl.M3u8Download(c['videos'][0]['main'], 'output/'+ course.json()['data']['name_zh'] + ('-screen' if vga == '2' else '-video'), name)
 
 

e68693d96f543c65b806b35fa916626f6d4fb544
AuYang261 <459461160@qq.com>
2024-05-31 22:10:41 +0800

    请求头增加Referer防止返回403

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ from hashlib import md5
 
 headers = {
     "Origin": "https://www.yanhekt.cn",
+    "Referer": "https://www.yanhekt.cn/",
     "xdomain-client": "web_user",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.26",
 }
